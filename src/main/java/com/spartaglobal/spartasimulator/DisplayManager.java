ce9224381420b002a45ddd857dc1b99ddbf298ba
Alvaro <63067669+Alvaropz@users.noreply.github.com>
2022-02-14 19:32:29 +0000

    DisplayManager updated
    printSystemInfo() updated so now it will print the basic information sentences once the simulation has finished. It has to be implemented the actual values.

--- src/main/java/com/spartaglobal/spartasimulator/DisplayManager.java
+++ src/main/java/com/spartaglobal/spartasimulator/DisplayManager.java
@@ -11,7 +11,13 @@ public class DisplayManager {
 
     public enum Message{
         MONTHS("Please, enter the simulation length in months: "),
-        INVALID_INPUT_MONTHS("Invalid input. " + MONTHS.message);
+        INVALID_INPUT_MONTHS("Invalid input. " + MONTHS.message),
+        TOTAL_MONTHS("Run the System for " + "<numberOfMonths>" + " months."),
+        CENTRES_OPEN("Number of open centres open: "),
+        FULL_CENTRES("Number of full centres: "),
+        TRAINEES_TRAINING("Number of trainees currently training: "),
+        TRAINEES_WAITING("Number of trainees on the waiting list: "),
+        SIMULATION_COMPLETE("Simulation complete");
 
         public final String message;
         Message(String message) {
@@ -26,8 +32,11 @@ public class DisplayManager {
         return input;
     }
 
-    public static void printSystemInfo(Simulation sim){
-
+    public static void printSystemInfo(TraineeDAO traineeDao){
+        System.out.println(Message.CENTRES_OPEN);
+        System.out.println(Message.FULL_CENTRES);
+        System.out.println(Message.TRAINEES_TRAINING);
+        System.out.println(Message.TRAINEES_WAITING);
     }
 
     public static void printException(Exception e){
