bf9d03329267ca6263d67c9cdaa3ae918eeb79f5
AntonyA3 <41267884+AntonyA3@users.noreply.github.com>
2022-02-15 10:14:54 +0000

    Made Changes to the TraineeDAO

--- src/main/java/com/spartaglobal/spartasimulator/Trainee.java
+++ src/main/java/com/spartaglobal/spartasimulator/Trainee.java
@@ -3,7 +3,7 @@ package com.spartaglobal.spartasimulator;
 public class Trainee {
     private int traineeID;
     public Trainee(int traineeID){
-
+        this.traineeID = traineeID;
     }
     public int getTraineeID(){
         return this.traineeID;
