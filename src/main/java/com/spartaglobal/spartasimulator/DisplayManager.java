5da62801cdfed6168c55bc6ba0038063c3a21b1b
Alvaro <63067669+Alvaropz@users.noreply.github.com>
2022-02-15 11:00:53 +0000

    DipslayManager update
    printSystemInfo() updated. Information displayed should work now.

--- src/main/java/com/spartaglobal/spartasimulator/DisplayManager.java
+++ src/main/java/com/spartaglobal/spartasimulator/DisplayManager.java
@@ -7,20 +7,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class DisplayManager {
-    public static void main(String[] args) {
-        TraineeDAO tdao = new TraineeDAO();
-        tdao.openConnection();
-        tdao.createTables();
-        tdao.addTrainee(new Trainee(12));
-        tdao.addTrainee(new Trainee(23));
-        tdao.addTrainee(new Trainee(24));
-        tdao.addTrainee(new Trainee(25));
-
-        tdao.addTrainingCentre( new TrainingCentre(12, 100));
-        System.out.println(tdao.getWaitingTrainees(false).length);
-        System.out.println("Waiting trainees " + tdao.getWaitingTrainees(false).length);
-        tdao.closeConnection();
-    }
 
     private static Logger logger = LogManager.getLogger("Display Manager");
 
@@ -50,8 +36,8 @@ public class DisplayManager {
     public static void printSystemInfo(TraineeDAO traineeDao){
         System.out.println(String.format(Message.CENTRES_OPEN.message, Arrays.stream(traineeDao.getCentreCapacities()).filter(t -> t > 0)));
         System.out.println(String.format(Message.FULL_CENTRES.message, Arrays.stream(traineeDao.getCentreCapacities()).filter(t -> t == 0)));
-        System.out.println(String.format(Message.TRAINEES_TRAINING.message, Arrays.stream(traineeDao.getTrainingTrainees()).count()));
-        System.out.println(String.format(Message.TRAINEES_WAITING.message, Arrays.stream(traineeDao.getWaitingTrainees(true)).count()));
+        System.out.println(String.format(Message.TRAINEES_TRAINING.message, traineeDao.getTrainingTrainees().length));
+        System.out.println(String.format(Message.TRAINEES_WAITING.message, traineeDao.getWaitingTrainees(false).length));
     }
 
     public static void printException(Exception e){
