1dc610c0447c0b45d73d5331e7b335735286cb7c
Alvaro <63067669+Alvaropz@users.noreply.github.com>
2022-02-15 10:54:54 +0000

    DisplayManager update

--- src/main/java/com/spartaglobal/spartasimulator/DisplayManager.java
+++ src/main/java/com/spartaglobal/spartasimulator/DisplayManager.java
@@ -3,20 +3,35 @@ package com.spartaglobal.spartasimulator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DisplayManager {
+    public static void main(String[] args) {
+        TraineeDAO tdao = new TraineeDAO();
+        tdao.openConnection();
+        tdao.createTables();
+        tdao.addTrainee(new Trainee(12));
+        tdao.addTrainee(new Trainee(23));
+        tdao.addTrainee(new Trainee(24));
+        tdao.addTrainee(new Trainee(25));
+
+        tdao.addTrainingCentre( new TrainingCentre(12, 100));
+        System.out.println(tdao.getWaitingTrainees(false).length);
+        System.out.println("Waiting trainees " + tdao.getWaitingTrainees(false).length);
+        tdao.closeConnection();
+    }
 
     private static Logger logger = LogManager.getLogger("Display Manager");
 
     public enum Message{
         MONTHS("Please, enter the simulation length in months: "),
         INVALID_INPUT_MONTHS("Invalid input. " + MONTHS.message),
-//        TOTAL_MONTHS("Run the System for " + "<numberOfMonths>" + " months."),
-        CENTRES_OPEN("Number of open centres open: "),
-        FULL_CENTRES("Number of full centres: "),
-        TRAINEES_TRAINING("Number of trainees currently training: "),
-        TRAINEES_WAITING("Number of trainees on the waiting list: "),
+        TOTAL_MONTHS("Run the System for %d months."),
+        CENTRES_OPEN("Number of open centres open: %d"),
+        FULL_CENTRES("Number of full centres: %d"),
+        TRAINEES_TRAINING("Number of trainees currently training: %d"),
+        TRAINEES_WAITING("Number of trainees on the waiting list: %d"),
         SIMULATION_COMPLETE("Simulation complete");
 
         public final String message;
@@ -32,13 +47,12 @@ public class DisplayManager {
         return input;
     }
 
-    // Thing to work on
-//    public static void printSystemInfo(TraineeDAO traineeDao){
-//        System.out.println(Message.CENTRES_OPEN);
-//        System.out.println(Message.FULL_CENTRES);
-//        System.out.println(Message.TRAINEES_TRAINING);
-//        System.out.println(Message.TRAINEES_WAITING);
-//    }
+    public static void printSystemInfo(TraineeDAO traineeDao){
+        System.out.println(String.format(Message.CENTRES_OPEN.message, Arrays.stream(traineeDao.getCentreCapacities()).filter(t -> t > 0)));
+        System.out.println(String.format(Message.FULL_CENTRES.message, Arrays.stream(traineeDao.getCentreCapacities()).filter(t -> t == 0)));
+        System.out.println(String.format(Message.TRAINEES_TRAINING.message, Arrays.stream(traineeDao.getTrainingTrainees()).count()));
+        System.out.println(String.format(Message.TRAINEES_WAITING.message, Arrays.stream(traineeDao.getWaitingTrainees(true)).count()));
+    }
 
     public static void printException(Exception e){
         logger.error(e);
--- src/main/java/com/spartaglobal/spartasimulator/Simulation.java
+++ src/main/java/com/spartaglobal/spartasimulator/Simulation.java
@@ -5,19 +5,19 @@ import java.util.Random;
 
 public class Simulation {
 
-    private static final int MIN_GENERATED_TRAINEES = 50;
-    private static final int MAX_GENERATED_TRAINEES = 100;
-    private static final int DEFAULT_CENTRE_CAPACITY = 100;
-    private static final int MAX_TRAINEES_CENTRE_TAKE = 50;
+//    private static final int MIN_GENERATED_TRAINEES = 50;
+//    private static final int MAX_GENERATED_TRAINEES = 100;
+//    private static final int DEFAULT_CENTRE_CAPACITY = 100;
+//    private static final int MAX_TRAINEES_CENTRE_TAKE = 50;
+//
+//    public static void simulate(int months, TraineeDAO tdao){
+//        TraineeFactory tf = new TraineeFactory();
+//        for(int i = 0; i < months; i++) loop(i, tdao, tf);
+//    }
 
-    public static void simulate(int months, TraineeDAO tdao){
-        TraineeFactory tf = new TraineeFactory();
-        for(int i = 0; i < months; i++) loop(i, tdao, tf);
-    }
-
-    private static void loop(int month, TraineeDAO tdao, TraineeFactory tf){
-        if((month % 2) == 1) tdao.addTrainingCentre(DEFAULT_CENTRE_CAPACITY);
-        Arrays.stream(tdao.getWaitingTrainees(true)).forEach(t -> tdao.addTrainee(t, MAX_TRAINEES_CENTRE_TAKE));
-        Arrays.stream(tf.getNewTrainees(MIN_GENERATED_TRAINEES, MAX_GENERATED_TRAINEES)).forEach(t -> tdao.addTrainee(t, MAX_TRAINEES_CENTRE_TAKE));
-    }
+//    private static void loop(int month, TraineeDAO tdao, TraineeFactory tf){
+//        if((month % 2) == 1) tdao.addTrainingCentre(DEFAULT_CENTRE_CAPACITY);
+//        Arrays.stream(tdao.getWaitingTrainees(true)).forEach(t -> tdao.addTrainee(t, MAX_TRAINEES_CENTRE_TAKE));
+//        Arrays.stream(tf.getNewTrainees(MIN_GENERATED_TRAINEES, MAX_GENERATED_TRAINEES)).forEach(t -> tdao.addTrainee(t, MAX_TRAINEES_CENTRE_TAKE));
+//    }
 }
