3bf868860b07b3ee1e1394c31b1fcb90b236031a
Autumn Pelešová <autumnlara@hotmail.com>
2022-02-17 17:22:40 +0000

    Fixed centre type string format for consistency

--- src/main/java/com/spartaglobal/spartasimulator/BootCamp.java
+++ src/main/java/com/spartaglobal/spartasimulator/BootCamp.java
@@ -1,27 +1,14 @@
 package com.spartaglobal.spartasimulator;
 
 public class BootCamp extends TrainingCentre{
-
-//<<<<<<< HEAD
-//    public BootCamp(int trainingCentreID) { super(trainingCentreID, 500); }
-//
-
-//=======
-
     private int monthsBelowThreshold;
 
     public BootCamp(int trainingCentreID, boolean isOpen, int monthsBelowThreshold) {
-        super(trainingCentreID, 500, "Boot Camp", isOpen);
+        super(trainingCentreID, 500, "BOOTCAMP", isOpen);
         this.monthsBelowThreshold = monthsBelowThreshold;
     }
 
     public int getMonthsBelowThreshold() { return monthsBelowThreshold; }
 
     public void setMonthsBelowThreshold(int monthsBelowThreshold) { this.monthsBelowThreshold = monthsBelowThreshold; }
-
-    @Override
-    public String getTrainingCentreCatagory() {
-        return "BOOT CAMP";
-    }
-//>>>>>>> 7c2b994c422d8fe04eb4819360d81d12b7770007
 }
